074298cec3bb9e9275fa0e246a8fad6c5954cadc
sebavides <sebavides@gmail.com>
2015-12-25 21:25:26 -0300

    Update

--- ISBN/ViewController.swift
+++ ISBN/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textoIsbn: UITextField!
+    @IBOutlet weak var visorResultado: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,44 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+        func sincrono(){
+            let isbn = textoIsbn.text
+            let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:\(isbn!)"
+            let url = NSURL(string: urls)
+            let datos:NSData? = NSData(contentsOfURL: url!)
+            
+            if datos == nil{
+                let alerta = UIAlertController(title: "Alerta", message: "No dispones de conexion a internet", preferredStyle: .Alert)
+            alerta.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+                self.presentViewController(alerta, animated: true, completion: nil)
+            }else{
+                let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+                visorResultado.text = String(texto)
+            }}
+        
+        
+    
+    @IBAction func searchTextField(sender: UITextField) {
+    if textoIsbn == ""{
+    let alerta = UIAlertController(title: "Cuidado", message: "Ingrese un ISBN", preferredStyle: .Alert)
+        alerta.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        self.presentViewController(alerta
+            , animated: true, completion: nil)
+    }else{
+        sincrono()
+        sender.resignFirstResponder()
+        }
+        }
+    @IBAction func desaparecerTeclado(sender: AnyObject){
+        textoIsbn.resignFirstResponder()
+        visorResultado.resignFirstResponder()
 
 }
+    
+    @IBAction func limpiar(sender: UIButton) {
+        textoIsbn.text = String()
+        visorResultado.text = String()
+    }
+}
+
 
